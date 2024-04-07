2238c08b2136ef5f76c4bb33b7b3144c3198e14b
pak-Z-Coder <zeeshiiscool@gmail.com>
2024-04-07 19:22:59 +0500

    Player controls fix 2

--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -322,11 +322,11 @@ const VideoPlayer = ({ Url, tracks, type, intro, outro, setEpEnded, userPreferen
                 qualityLevel: qualities[qIndex]?.height
             }));
         }
-        setPlaying(true)
+        
     }
     return (
         <div id="player" className={cn('z-0 relative w-full h-full')}>
-            <div id="playerAbsolute" className='z-0 absolute h-[85%] w-full'></div>
+            <div id="playerAbsolute" className='z-10 absolute h-[85%] w-full'></div>
             <ReactPlayer
                 ref={player}
                 volume={volume}
@@ -363,8 +363,7 @@ const VideoPlayer = ({ Url, tracks, type, intro, outro, setEpEnded, userPreferen
                 {playing ?
                     <Pause className="max-w-6 max-h-6" /> : <Play className="w-6 h-6" />}
             </Button>
-            <div className={cn("z-10 absolute h-full w-full inset-0 opacity-100 flex flex-col justify-end ", !showControls && !isOpen && !isOpen1 && "opacity-0 hidden transition-opacity ease-out")} >
-                <div className="w-full bg-black/20">
+            <div className={cn("w-full absolute opacity-100 block bottom-0 z-20 bg-black/20", !showControls && !isOpen && !isOpen1 && "opacity-0 hidden transition-opacity ease-out")}>
                     <div className="mb-2 pb-2 relative flex items-center justify-around max-w-full font-semibold text-xs text-white textStrokeSmall pt-3 overflow-hidden">
                         <p>{currentTime ? formatTime(currentTime) : "00:00:00"}</p>
                         <Slider value={[currentTime]} min={0} max={duration} loadedTime={loadedTime} onValueChange={(e) => handleSeek(e)} step={10} className="w-[90%]" />
@@ -419,8 +418,6 @@ const VideoPlayer = ({ Url, tracks, type, intro, outro, setEpEnded, userPreferen
                     </div>
 
                 </div>
-            </div>
-
         </div >
     )
 }
